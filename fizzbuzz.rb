244ff121316ad7fb9572b5fdc9f8ad3711375a02
balanced-widget-4002 <jessikim807@gmail.com>
2020-02-01 00:48:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(integer)
-  if intger % 3 == 0 && integer % 5 == 0
+  if integer % 3 == 0 && integer % 5 == 0
     puts "FizzBuzz"
+  elsif integer % 3 == 0 
+    puts "Fizz"
+  elsif integer % 5 == 0 
+    puts "Buzz"
   end
 end
