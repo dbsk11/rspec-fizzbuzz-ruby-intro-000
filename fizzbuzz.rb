eb03831253e01a47de88c9c7e4d41cf4083693f0
balanced-widget-4002 <jessikim807@gmail.com>
2020-02-01 00:44:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-  if number % 3 == 0
-    puts "Fizz"
-  else
-    puts "nil"
+
 end
