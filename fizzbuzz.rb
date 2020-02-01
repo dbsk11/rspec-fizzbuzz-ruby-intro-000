173a3c31676c762fe26046b1467e721460966154
balanced-widget-4002 <jessikim807@gmail.com>
2020-02-01 00:27:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
     "FizzBuzz"
-  elsif number % 3 == 0 
+  elsif number % 3 == 0
     "Fizz"
-  elsif number % 5 == 0 
+  elsif number % 5 == 0
     "Buzz"
   else
     number
